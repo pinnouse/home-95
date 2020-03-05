09e0b34a8f13a4c1d9a1655a867931457501dd84
Nick <cupofpho@gmail.com>
2020-03-05 00:03:20 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m "deploy"
 
-git push -f https://github.com/pinnouse/home.git master:gh-pages
+git push -f https://github.com/pinnouse/home-95.git master:gh-pages
 
 cd -
